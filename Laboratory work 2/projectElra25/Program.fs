42606bc2fb905c0ad5aa4b41a0a330b3a869e278
roge111 <64649951+roge111@users.noreply.github.com>
2024-11-11 14:26:03 +0300

    Добавление папки с программой на 25 проблему

---
+++ Laboratory work 2/projectElra25/Program.fs
@@ -0,0 +1,25 @@
+﻿module projectElra25
+// For more information see https://aka.ms/fsharp-console-apps
+
+
+// Реализация модульной реализации с хвостовой рекурсией
+let fibonachiHvost (i: int): int = 
+    let rec loop (acc: int list, i)  = 
+            
+            let mutable count = acc.Length
+            if acc[count-1] >= i then acc[count-1]
+            else
+                loop (List.append acc [acc[count-2] + acc[count - 1]], i)
+    
+    loop ([1; 1], i)
+
+
+
+// Реализация рекурсивной программой
+let rec loop_r(array:int list, i: int): int =
+    let mutable lastIndex = array.Length - 1
+    if array[lastIndex] >= i then array[lastIndex]
+    else
+        loop_r(List.append array [array[lastIndex - 1] + array[lastIndex]], i)
+
+
